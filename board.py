699a3865b05c8385a8a637c84026139e2a60c723
Ridwan Hady <ridwanhady31@gmail.com>
2019-09-16 15:01:37 +0700

    fixin method

--- board.py
+++ board.py
@@ -1,13 +1,13 @@
 import constant
 class Board:
     def __init__(self):
-        initializeState()
+        self.__initializeState()
 
-    def initializeState(self, size):
+    def __initializeState(self):
         self.state = [[ constant.EMPTY for row in range(constant.SIZE)] for column in range(constant.SIZE)]
-        initializeStartPiece()
+        self.__initializeStartPiece()
     
-    def initializeStartPiece(self):
+    def __initializeStartPiece(self):
         self.state[3][3] = constant.WHITE
         self.state[4][4] = constant.WHITE
         self.state[4][3] = constant.BLACK
