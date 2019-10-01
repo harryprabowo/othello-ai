5bde020c5089dc59145b7a68bcba93176258d157
Ridwan Hady <13517007@std.stei.itb.ac.id>
2019-10-01 19:01:57 +0700

    Fixing conflict

--- step.py
+++
@@ -1,8 +0,0 @@
-class Step:
-    def __init__(self, row, column, piece):
-        self.row = row
-        self.column = column
-        self.piece = piece
-
-    def get_state_value(self):
-        return self.row * 10 + self.column;
