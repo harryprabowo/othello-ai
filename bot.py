24aa947a3755ebec6e6751c7abe5613fcd2761cc
Firastic <adyaksa.wisanggeni@gmail.com>
2019-10-01 11:57:29 +0700

    Fix min max

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ import math
 from board import *
 
 class Bot:
-    def __init__(self, piece_color):
+    def __init__(self, board, piece_color):
+        self.board = board
         self.piece_color = piece_color
         self.depth = constant.DEFAULT_DEPTH
 
@@ -11,30 +12,33 @@ class Bot:
         return self.dfs(current_state, self.piece_color, self.depth, -math.inf, math.inf)
 
     def dfs(self, current_state, current_player, remaining_depth, alpha, beta):
-        print(remaining_depth)
         if remaining_depth == 0:
-            return self.__evaluate_state(current_state)
+            return self.__evaluate_state(current_state), None
         else:
-            possible_step = self.__generate_possible_step(current_state, current_player)
+            possible_step = self.board.possible_moves(current_player)
             if not possible_step:
-                if not self.__generate_possible_step(current_state, self.__enemy_of(current_player)):
-                    print(self.__final_value(current_state))
-                    return self.__final_value(current_state)
+                if not self.board.possible_moves(self.__enemy_of(current_player)):
+                    return self.__final_value(current_state), None
                 else:
-                    return -self.dfs(current_state, self.__enemy_of(current_player), remaining_depth - 1, -beta, -alpha)
+                    return -self.dfs(current_state, self.__enemy_of(current_player), remaining_depth - 1, -beta, -alpha)[0], None
             best_value = -999 if (self.piece_color == current_player) else 999
+            best_move = 0
             for step in possible_step:
-                next_state = self.__change_state(step, current_state)
-                value = -self.dfs(next_state, self.__enemy_of(current_player), remaining_depth - 1, -beta, -alpha)
+                next_state = self.__change_state(step, current_state, current_player)
+                value = -self.dfs(next_state, self.__enemy_of(current_player), remaining_depth - 1, -beta, -alpha)[0]
                 if current_player == self.piece_color:
-                    best_value = max(best_value, value)
-                    alpha = max(alpha, value)
+                    if best_value < value:
+                        best_value = value
+                        alpha = value
+                        best_move = step
                 else:
-                    best_value = min(best_value, value)
-                    beta = min(beta, value)
+                    if best_value > value:
+                        best_value = value
+                        beta = value
+                        best_move = step
                 if beta <= alpha:
                     break
-            return best_value
+            return best_value, best_move
 
     def __final_value(self, state):
         score = self.__score(state)
@@ -51,10 +55,6 @@ class Bot:
     def __square_list(self):
         return [i for i in range(11, 89) if 1 <= (i % 10) <= 8]
 
-    def __generate_possible_step(self, current_state, current_player):
-        # TODO
-        return []
-
     def __evaluate_state(self, state):
         res = 0
         for square in self.__square_list():
@@ -62,8 +62,8 @@ class Bot:
             res += constant.BOARD_VALUE[square] * modifier
         return res
 
-    def __change_state(self, step, state):
-        state[step.getStateValue()] = step.piece
+    def __change_state(self, move, state, piece):
+        state[move] = piece
         return state
 
     def __enemy_of(self, current_player):
